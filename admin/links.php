384058eadaa066fdb8cfb1858c5eeecae6a90e0d
laety-seri <63522571+laety-seri@users.noreply.github.com>
2021-03-24 17:47:55 +0000

    userpage

--- admin/links.php
+++ admin/links.php
@@ -7,4 +7,4 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="https://kit.fontawesome.com/7cb0e7c261.js" crossorigin="anonymous"></script>
-<script src="scripts.js"></script>
+<script src="../admin/scripts.js"></script>
