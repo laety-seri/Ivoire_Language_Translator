071cfdc1f181f083cf01612321be0e1cb1435026
laety-seri <63522571+laety-seri@users.noreply.github.com>
2021-03-24 22:28:49 +0000

    trad

--- public/index.php
+++ public/index.php
@@ -19,15 +19,15 @@
     <body>
 
       <header>
-          <center> <h3 class="text-success"><a href="userspage.php"> IVOIRE LANGUAGE TRANSLATOR </a></h3>
-          <a href="#"><img src="../admin/logo.png" alt="logo" height="160" width="160"></a>
-          </center>
+        <center> <h3 class="text-success"><a href="index.php"> IVOIRE LANGUAGE TRANSLATOR </a></h3>
+        <a href="#"><img src="../admin/logo.png" alt="logo" height="160" width="160"></a>
+        </center>
       </header>
 <!-- espace traduction  test-->
 <div class="container traduction"> <br>
-    <form action="" method="POST" enctype="multipart/form-data" onsubmit="checkall(); " >
+  <form action="" method="POST" enctype="multipart/form-data" onsubmit="checkall(); " >
     <div class="row">
-        <div class="col col-sm-6 ">
+      <div class="col col-sm-6 ">
         <select class="form-control" name ="langue_start" id ="langue_start">
           <option value="" class="" > Sélectionner la langue d'origine </option>
             <?php
@@ -37,9 +37,9 @@
                 echo '<option>'.$row['langue'].'</option>';
               }
             ?> 
-          </select>
-        </div>
-        <div class="col col-sm-6">
+        </select>
+      </div>
+      <div class="col col-sm-6">
         <select class="form-control" name ="langue_end" id ="langue_end">
           <option value="" class="" > Sélectionner la langue de traduction </option>
             <?php
@@ -49,19 +49,18 @@
                 echo '<option>'.$row['langue'].'</option>';
               }
             ?> 
-          </select>
-          <br><br>
+          </select> <br><br>
         </div>
       </div>
       <div class="row">
         <div class="col col-sm-4">
-          <textarea class="textarea" rows="8" cols="40"  name="champ1" id="champ1" onkeyup="check();"></textarea>
+          <textarea class="textarea form-control" rows="8" cols="40"  name="champ1" id="champ1" onkeyup="check();"></textarea>
         </div>
         <div class="col col-sm-4">
-            <input type="submit" value="Traduire" name="Traduire" class="btn btn-success" onclick="reload();">
+            <input type="submit" value="Traduire" name="Traduire" class="btn btn-success form-control" onclick="reload();">
         </div> 
           <div class="col col-sm-4">
-              <textarea class="textarea" rows="8" cols="40"  id="champ1status" disabled>                    
+              <textarea class="textarea form-control" rows="8" cols="40"  id="champ1status" disabled>                    
                 <?php
                 if (isset($_POST['Traduire'])) {
                 $search_text = $_POST['champ1'];
