b5bc5310ad3e735168c5ecf7100d3234aa4e1afe
laety-seri <63522571+laety-seri@users.noreply.github.com>
2021-03-24 20:46:59 +0000

    trad

--- admin/scripts.js
+++ admin/scripts.js
@@ -5,3 +5,56 @@ $(document).ready( function () {
 $(document).ready( function () {
     $('#trad2').dataTable();
     } );
+
+    function check() {
+        // ... ajouter la fonction ajax ... //
+    
+        var champ1=document.getElementById( "champ1" ).value;
+            
+        if(champ1)
+        {
+            $.ajax({
+                type: 'POST',
+                url: '../admin/checkdata.php',
+                data: {
+                champ1:champ1,
+                },
+                success: function (response) {
+                    $( '#champ1status' ).html(response);
+                    if(response == "")	
+                    {
+                        return true;	
+                    }
+                    else
+                    {
+                        return false;	
+                    }
+                }
+            });
+        }
+            else
+            {
+                $( '#champ1status' ).html("");
+                return false;
+            }
+    }
+    
+    function checkall()
+    {
+        var texte1html=document.getElementById("champ1status").innerHTML;
+    
+        if((champ1html) == "")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    
+    }
+    function reload() {
+      window.location.reload();
+      return false;
+    }
+    
--- public/index.php
+++ public/index.php
@@ -106,64 +106,10 @@
                 <div>
                     <p> <font color = 'red'> Traduction non disponible <br> <a href="addtextusers.php">Soumettre une proposition de traduction</a></p>
                 </div>
-    <?php }}} ?>
-    </div>
-    </form>
+              </div>
+            </form>
+            <?php }}} ?>
 
   </div>
-<script>
-  function check() {
-    // ... ajouter la fonction ajax ... //
-
-    var champ1=document.getElementById( "champ1" ).value;
-        
-    if(champ1)
-    {
-        $.ajax({
-            type: 'POST',
-            url: '../admin/checkdata.php',
-            data: {
-            champ1:champ1,
-            },
-            success: function (response) {
-                $( '#champ1status' ).html(response);
-                if(response == "")	
-                {
-                    return true;	
-                }
-                else
-                {
-                    return false;	
-                }
-            }
-        });
-    }
-        else
-        {
-            $( '#champ1status' ).html("");
-            return false;
-        }
-}
-
-function checkall()
-{
-    var texte1html=document.getElementById("champ1status").innerHTML;
-
-    if((champ1html) == "")
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-
-}
-function reload() {
-  window.location.reload();
-  return false;
-}
-
-</script>
 </body>
 </html>
